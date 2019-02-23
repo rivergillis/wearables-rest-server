f5a1f91ee5ab9891bb2176d36f1711000349dd1e
River Gillis <jrivergillis@gmail.com>
2019-02-23 16:57:51 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// These needs to be the first thin imported in the app
+import "dotenv/config";
+
+import http from "http";
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(process.env.MY_SECRET);
+  })
+  .listen(1337, "127.0.0.1");
+
+console.log("Server running at http://127.0.0.1:1337/");
