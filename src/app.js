6fe34f31641acf5b46ec939e40cca538fcfea84f
River Gillis <jrivergillis@gmail.com>
2019-02-23 20:35:45 -0600

    Add user signup, login, deletion, listing

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
 // handle errors from anywhere in the app
 app.use((error, req, res, next) => {
-  console.log("Error handler from app.js");
+  console.error(error.stack);
   res.status(error.status || 500);
   res.json({ error: { message: error.message } });
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,58 +2,82 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-// const User = require("../models/user");
+import User from "../models/user";
 
 const router = express.Router();
 
+// GET /users/
+// Returns a list of every user (passwords hidden)
+router.get("/", async (req, res, next) => {
+  try {
+    const docs = await User.find()
+      .select("_id email")
+      .exec();
+    const response = { count: docs.length, users: docs };
+    return res.status(200).json(response);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// POST /users/signup with a User (email, password)
+// Signs up a new user.
 router.post("/signup", async (req, res, next) => {
-  // try {
-  //   const foundUser = await User.find({ email: req.body.email });
-  //   if (foundUser.length >= 1) {
-  //     return res.status(409).json({ message: "Mail exists" });
-  //   }
-  //   // No user found, continue. Salt and hash the password then save.
-  //   const hash = await bcrypt.hash(req.body.password, 10);
-  //   const user = new User({ email: req.body.email, password: hash });
-  //   const result = await user.save();
-  //   console.log(result);
-  //   res.status(201).json({ message: "User created" });
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).json({ error: err });
-  // }
+  try {
+    // Try to find a matching email to make sure we don't double-signup
+    const foundUser = await User.find({ email: req.body.email });
+    if (foundUser.length >= 1) {
+      return res.status(409).json({ message: "Mail exists" });
+    }
+
+    // No user found, continue. Salt and hash the password then save.
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const user = new User({ email: req.body.email, password: hash });
+    const result = await user.save();
+
+    console.log(result);
+    res.status(201).json({ message: "User created" });
+  } catch (err) {
+    return next(err);
+  }
 });
 
+// POST /users/login with a User (email, password)
+// Logs the user in.
+// Returns a JWT (auth token) to be used for future requests.
 router.post("/login", async (req, res, next) => {
-  // try {
-  //   const user = await User.find({ email: req.body.email }).exec();
-  //   if (user.length < 1) {
-  //     return res.status(401).json({ message: "Auth failed" });
-  //   }
-  //   const result = await bcrypt.compare(req.body.password, user[0].password);
-  //   if (!result) {
-  //     return res.status(401).json({ message: "Auth failed" });
-  //   }
-  //   // Generate a JSON web token to use for auth.
-  //   const token = jwt.sign(
-  //     { email: user[0].email, userId: user[0]._id },
-  //     process.env.JWT_KEY,
-  //     { expiresIn: "1h" }
-  //   );
-  //   return res.status(200).json({ message: "auth successful", token: token });
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).json({ error: err });
-  // }
+  try {
+    // Attempt to find the user with with matching email.
+    const user = await User.findOne({ email: req.body.email }).exec();
+    if (!user) {
+      return res.status(401).json({ message: "Auth failed" });
+    }
+    // Compare the passwords
+    const result = await bcrypt.compare(req.body.password, user.password);
+    if (!result) {
+      return res.status(401).json({ message: "Auth failed" });
+    }
+    // Generate a JSON web token to use for auth based on the email and id.
+    const token = jwt.sign(
+      { email: user.email, userId: user._id },
+      process.env.JWT_KEY,
+      { expiresIn: "24h" }
+    );
+    return res.status(200).json({ message: "auth successful", token: token });
+  } catch (err) {
+    next(err);
+  }
 });
 
+// DELETE /users/:userId
+// Deletes the user with id of userID
 router.delete("/:userId", async (req, res, next) => {
-  // try {
-  //   const result = await User.deleteOne({ _id: req.params.userId }).exec();
-  //   return res.status(200).json({ message: "user deleted" });
-  // } catch (err) {
-  //   res.status(500).json({ error: err });
-  // }
+  try {
+    await User.deleteOne({ _id: req.params.userId }).exec();
+    return res.status(200).json({ message: "user deleted" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
