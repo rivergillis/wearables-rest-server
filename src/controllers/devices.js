1c8b0d25ec98f3f7fdd21a04d1538419ff68716c
River Gillis <jrivergillis@gmail.com>
2019-02-28 16:04:36 -0600

    update getting devices

--- src/controllers/devices.js
+++ src/controllers/devices.js
@@ -5,16 +5,22 @@ import Device from "../models/device";
 
 export const get_all_devices = async (req, res, next) => {
   try {
-    // TODO: check this to get devices only for the current logged in user
-    // we get the user email from checkauth, add the use email to the device
-    // as an owner when it is created, then search for that
-
-    // TODO: change this to accept a 'type' in the body that determines whether
-    // to get the list of devices that the user has read or write access to
-
-    const docs = await Device.find()
-      .select("-__v")
-      .exec();
+    let docs = null;
+    if (req.body.type === "owner") {
+      docs = await Device.find({ ownerId: req.userData.userId })
+        .select("-__v")
+        .exec();
+    } else if (req.body.type === "admin") {
+      // TODO: protect this somehow
+      docs = await Device.find()
+        .select("-__v")
+        .exec();
+    } else if (req.body.type == "read") {
+      // TODO: Search for devices with read access
+      throw new Error("Not yet implemented");
+    } else {
+      throw new Error("Invalid type property");
+    }
     const response = { count: docs.length, devices: docs };
     return res.status(200).json(response);
   } catch (err) {
@@ -31,8 +37,7 @@ export const create_new_device = async (req, res, next) => {
     });
     const result = await device.save();
 
-    // TODO: alter the user to add result._id to their 'writes' list
-    // Also add the user email to the device as an 'owner'
+    // TODO: alter the user to add result._id to their 'writes' list?
 
     console.log(result);
     res.status(201).json({ message: "Device created" });
--- src/routes/devices.js
+++ src/routes/devices.js
@@ -6,10 +6,13 @@ import * as DevicesController from "../controllers/devices";
 const router = express.Router();
 
 // GET /devices/
+// HEADER: bearer auth
+// BODY: type (str -> options: 'owner', 'admin')
 // Returns a list of every device
-router.get("/", DevicesController.get_all_devices);
+router.get("/", checkAuth, DevicesController.get_all_devices);
 
 // POST /users/ with a Device
+// HEADER: bearer auth
 // BODY: name (str), timeout (str of zeit/ms format https://www.npmjs.com/package/ms)
 // Creates a new device listing
 router.post("/", checkAuth, DevicesController.create_new_device);
